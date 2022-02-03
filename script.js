1ceb8ae3a859573f2ef55b557d76f7224ea63459
Bernd Ullmann <berndullmann@Bernds-MacBook-Air.fritz.box>
2022-02-03 14:03:01 +0100

    map is working now

--- script.js
+++ script.js
@@ -10,3 +10,50 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+// For real world applications 
+
+// 1. Start with user stories
+// Description of the application's funtionallity from the user's perspective. 
+// All user stories put togehter describe the entire application
+// Common format: As a [type of user], I want [an action] so that [a benefit]
+// [type of user]: Who? Example -> user, admin, etc.
+// [an action]: What?
+// [a benefit]: Why?
+
+// 2. Features
+// 3. Flowchart
+// What we will build
+
+// 4. Architecture
+// How we will build it
+
+// First Four points describe the "Planning Steps" then you start with "Development Steps"
+
+// navigator.gelocation is also good for munichclimbs!!!
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+        // This is the old way to write it. Will see it in the objects section (Destructoring)! 
+        //const latitude = position.coords.latitude;
+        const {latitude} = position.coords;
+        const {longitude} = position.coords;
+        console.log(latitude, longitude);
+        //console.log(`https://www.google.de/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+    }, function() {
+        alert('Could not get your position');
+    });
+}
