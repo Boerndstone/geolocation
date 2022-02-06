aae359d4351eb73de6419b1c42b016400d2af244
Bernd Ullmann <berndullmann@Bernds-Air.fritz.box>
2022-02-06 11:35:14 +0100

    marker rendering

--- script.js
+++ script.js
@@ -45,13 +45,38 @@ if(navigator.geolocation) {
 
         const map = L.map('map').setView(coords, 13);
 
+        // This is how to pass options to Leaflet markers
+        var markerOptions = {
+            clickable: true,
+            draggable: true,
+            riseOnHover: true,
+         }
+
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+        
+
+        map.on('click', function(mapEvent) {
+            console.log(mapEvent);
+            const {lat, lng} = mapEvent.latlng; 
+            //
+            L
+                .marker([lat, lng], markerOptions)
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup'
+                }))
+                .setPopupContent('Workout') 
+                .openPopup()
+            ;
+        });
 
     }, function() {
         alert('Could not get your position');
